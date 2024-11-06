5f15ecf4bddd029a4ad3719b6176190eac75d0b8
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-11-06 10:15:26 -0300

    setup LogManager class

---
+++ Enciclopedia/LogReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enciclopedia
+{
+    internal class LogManager
+    {
+        private static string? _mostAccessedMonster;
+        private static string? _mostAccessedCard;
+
+        public string? MostAccessedMonster => _mostAccessedMonster;
+        public string? MostAccessedCard => _mostAccessedCard;
+
+        public static void ComputeData(List<string?> accessLog)
+        {
+            if (accessLog == null) return;
+
+            for (int i = 0; i < accessLog.Count; i++)
+            {
+
+            }
+        }
+    }
+}
--- Enciclopedia/Program.cs
+++ Enciclopedia/Program.cs
@@ -1,10 +1,14 @@
 ﻿using Enciclopedia;
 
 string wikiPath = "data/wiki.txt";
+string logPath = "data/log.txt";
 
 List<string?> wikiList = FileReader.CreateList(wikiPath);
+List<string?> logList = FileReader.CreateList(logPath);
 
 TreeNode<string> homeNode = HierarchyManager.CreateHierarchyFromList(wikiList);
+
+LogManager.ComputeData(logList);
 
 homeNode.ProcessPreOrder(PrintNode);
 
